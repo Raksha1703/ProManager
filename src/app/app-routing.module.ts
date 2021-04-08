10097192a01fcb4dd605e65bc5da9314479122bd
Raksha1703 <rakshabarai173@gmail.com>
2021-04-08 15:51:39 +0530

    project overview complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FacultyComponent } from './faculty/faculty.component';
 import { LoginComponent } from './login/login.component';
+import { OpenproComponent } from './project/openpro/openpro.component';
 import { ProjectComponent } from './project/project.component';
 import { RegisterComponent } from './register/register.component';
 import { SettingComponent } from './setting/setting.component';
@@ -17,7 +18,8 @@ const routes: Routes = [
   {path:'faculty', component:FacultyComponent},
   {path: 'setting', component:SettingComponent},
   {path: 'student', component:StudentComponent},
-  {path:'project', component:ProjectComponent}
+  {path:'project', component:ProjectComponent},
+  {path: 'projectview', component:OpenproComponent}
 ];
 
 @NgModule({
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
