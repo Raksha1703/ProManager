2cbb2ef8055fa529749bc9ad9ace56e3342671c3
Raksha1703 <rakshabarai173@gmail.com>
2021-06-12 16:01:05 +0530

    profile service & modal added

--- src/app/faculty/faculty.component.ts
+++ src/app/faculty/faculty.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProfileService } from 'src/app/services/profile.service'; 
+import { Profile } from 'src/app/model/profile';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-faculty',
   templateUrl: './faculty.component.html',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacultyComponent implements OnInit {
   
-  constructor() { }
+  constructor(private profileservice:ProfileService,
+              private route:ActivatedRoute) { }
 
-  ngOnInit(): void {
+  //public users$:Observable<object>;
+  public users : any;
+  ngOnInit(): void {    
+    this.profileservice.getAll()
+       .subscribe((res:any) => {
+         this.users = res.faculty;
+         console.log(res.faculty)});
+     ;
+    // this.users$ = this.profileservice.getData();
+    // this.profileservice.getData()
+    //    .subscribe((response) => {
+    //     console.log('OBSERVE "response" RESPONSE is ', response);
+    //    });
   }
 
 }
---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+//import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
+import { Profile } from 'src/app/model/profile';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private http:HttpClient) { }
+
+  public url='http://127.0.0.1:8000/api/';
+  
+  getData() : Observable<Profile[]> {
+    return this.http.get<Profile[]>(this.url + 'profile');
+  }
+  getAll() : any {
+    return this.http.get<any>(this.url + 'profile');
+  }
+}
